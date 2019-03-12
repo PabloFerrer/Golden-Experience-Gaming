5c07e9a2e2b85c52fda269273ef8999796958f75
JavierRelinque <javierrelinque@hotmail.com>
2019-03-12 13:02:24 +0100

    añadido login y register automatico de laravel

--- Golden-Experience-Gaming/resources/views/welcome.blade.php
+++ Golden-Experience-Gaming/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    GORUDO EXPIRIENS
+                    GOLDEN EXPERIENCE GAMING
                 </div>
 
                 <div class="links">
