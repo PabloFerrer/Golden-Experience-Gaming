6eb16165e3bd0782c190c42a4482701b139ad452
JavierRelinque <javierrelinque@hotmail.com>
2019-03-19 12:40:14 +0100

    Menu lateral (primera version)

---
+++ Golden-Experience-Gaming/resources/views/includes/menu.blade.php
@@ -0,0 +1,24 @@
+<div class="card">
+	<div class="card-header">Menú</div>
+	
+	<div class="card-body">
+		<ul class="nav nav-pills nav-stacked">
+			<li>
+				<a href="/" class="list-group-item">
+					Bienvenido
+				</a>
+			</li>
+			<li>
+				<a href="/instrucciones" class="list-group-item">
+					Instrucciones
+				</a>
+			</li>
+			<li>
+				<a href="/acerca-de" class="list-group-item">
+					Créditos
+				</a>
+			</li>				
+		</ul>
+	</div>
+</div>
+
