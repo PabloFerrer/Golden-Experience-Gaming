bd5d1e82eeb6f519a9d45496bf0258e55a10a4c5
JavierRelinque <javierrelinque@hotmail.com>
2019-03-19 13:43:11 +0100

    Menu lateral (3a version)

--- Golden-Experience-Gaming/resources/views/layouts/app.blade.php
+++ Golden-Experience-Gaming/resources/views/layouts/app.blade.php
@@ -78,12 +78,16 @@
         </nav>
 
 
-		<div class="col-md-3">
-			@include('includes.menu')
-		</div>
-		<div class="col-md-9">
-			@yield('content')
-		</div>
+        <main class="py-4">
+				<div class="row">
+					<div class="col-md-3">
+						@include('includes.menu')
+					</div>
+					<div class="col-md-9">
+						@yield('content')
+					</div>
+				</div>            
+        </main>
 
     </div>
 	
