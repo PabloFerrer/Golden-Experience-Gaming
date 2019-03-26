8037b7dc91f7afe74a3f58e526d52de39308e1da
40CompilerErrors <danvgdesign@gmail.com>
2019-03-26 09:39:09 +0100

    Middleware Created

--- Golden-Experience-Gaming/routes/web.php
+++ Golden-Experience-Gaming/routes/web.php
@@ -6,3 +6,21 @@ Auth::routes();
 
 Route::get('/index', 'IndexController@index');
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Place routes for clients here
+Route::group(['middleware' => 'client'], function(){
+
+
+});
+
+//Place routes for publishers here here
+Route::group(['middleware' => 'publisher'], function(){
+
+
+});
+
+//Place routes for admins here here
+Route::group(['middleware' => 'admin'], function(){
+
+
+});
