964e5481a45fd032eae6d55b9d5118b786015003
JavierRelinque <javierrelinque@hotmail.com>
2019-03-26 12:50:14 +0100

    carrusel del banner

--- Golden-Experience-Gaming/resources/views/index.blade.php
+++ Golden-Experience-Gaming/resources/views/index.blade.php
@@ -13,7 +13,7 @@
 					</ol>
 					
 					<div class="carousel-inner">
-						<div class="item active">
+						<div class="carousel-item active">
 							<div class="bannergame">
 								@isset( $indexgames[0] )
 									{{ $indexgames[0]->name }}
@@ -24,7 +24,7 @@
 							</div>
 						</div>
 
-						<div class="item">
+						<div class="carousel-item">
 							<div class="bannergame">
 								@isset( $indexgames[1] )
 									{{ $indexgames[1]->name }}
@@ -35,7 +35,7 @@
 							</div>
 						</div>
 
-						<div class="item">
+						<div class="carousel-item">
 							<div class="bannergame">
 								@isset( $indexgames[2] )
 									{{ $indexgames[2]->name }}
@@ -46,7 +46,7 @@
 							</div>
 						</div>
 						
-						<div class="item">
+						<div class="carousel-item">
 							<div class="bannergame">
 								@isset( $indexgames[3] )
 									{{ $indexgames[3]->name }}
@@ -57,14 +57,14 @@
 							</div>
 						</div>
 					  </div>
-					 		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-							  <span class="glyphicon glyphicon-chevron-left"></span>
-							  <span class="sr-only">Previous</span>
-							</a>
-							<a class="right carousel-control" href="#myCarousel" data-slide="next">
-							  <span class="glyphicon glyphicon-chevron-right"></span>
-							  <span class="sr-only">Next</span>
-							</a>
+				  <a class="carousel-control-prev" href="#bannercarousel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				  </a>
+				  <a class="carousel-control-next" href="#bannercarousel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				  </a>
 				</div>
 				
 				<div class = "relleno">
