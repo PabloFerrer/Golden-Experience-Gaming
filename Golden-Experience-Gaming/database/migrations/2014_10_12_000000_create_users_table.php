0781a3f402f6f4b3551aaec1579f52b865afa3dd
40CompilerErrors <danvgdesign@gmail.com>
2019-03-26 13:58:55 +0100

    fix

--- Golden-Experience-Gaming/database/migrations/2014_10_12_000000_create_users_table.php
+++ Golden-Experience-Gaming/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->float('amount', 4, 2);
+            $table->float('wallet', 4, 2);
             $table->smallInteger('role')->default(1); // 0: Admin | 1: Client | 2: Publisher
             $table->rememberToken();
             $table->timestamps();
