ae985714d57edd1bc7582c605ef3285caca9045f
40CompilerErrors <danvgdesign@gmail.com>
2019-04-17 20:52:19 +0200

    Some css and html work

--- Golden-Experience-Gaming/resources/views/catalog.blade.php
+++ Golden-Experience-Gaming/resources/views/catalog.blade.php
@@ -4,22 +4,22 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-4">
-           
-           @isset( $indexgames[0] )
+
+      @isset( $indexgames[0] )
 				<a href="/game/{{$indexgames[0]->id}}">{{ $indexgames[0]->name }}</a>
-									
+
 			@else
-				NO HAY JUEGO 
+				NO HAY JUEGO
 			@endisset
         </div>
         <div class="col-md-4">
-            
-           @isset( $indexgames[1] )
+
+      @isset( $indexgames[1] )
 
 				<a href="/game/{{$indexgames[1]->id}}">{{ $indexgames[1]->name }}</a>
-									
+
 			@else
-				NO HAY JUEGO 
+				NO HAY JUEGO
 			@endisset
 			<br/>
 			<br/>
--- Golden-Experience-Gaming/resources/views/layouts/app.blade.php
+++ Golden-Experience-Gaming/resources/views/layouts/app.blade.php
@@ -17,97 +17,24 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel bar">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'GOLDEN EXPERIENCE GAMING') }}
-                </a>
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{asset('img/logo.jpeg')}}">
-                </a>
-                
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('cart') }}">
-										{{ __('Ver carrito') }}
-									</a>
-									
-									<a class="dropdown-item" href="{{ route('wishlist') }}">
-										{{ __('Ver lista de deseados') }}
-									</a>
-									
-									<a class="dropdown-item" href="{{ route('wallet') }}">
-										{{ __('Añadir fondos') }}
-									</a>
-									
-									<a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </div>
-
-
-                            </li>
-							<li id="navmoney" class="nav-item">
-								{{ Auth::user()->wallet }}
-							</li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
-        
-				<div class="row">
-					<div class="col-md-3">
-						@include('includes.menu')
-					</div>
-					<div class="col-md-9">
-						<main class="py-4">
-							@yield('content')
-						</main>
-					</div>
-				</div>            
-        
-
+<body background="{{ asset('img/background.png') }}">
+    <!-- <div id="app"> -->
+    @include('includes.topbar')
+    <div class="container-fluid" id="content-container">
+      <div class="row">
+  	     <div class="col-md-3">
+  		       @include('includes.menu')
+  	     </div>
+  			 <div class="col-md-9">
+  			      <main class="py-4">
+  			           @yield('content')
+  	          </main>
+  			 </div>
+  	  </div>
     </div>
-	
+    <!-- </div> -->
 </body>
 </html>
