89804b16a3211b29f5eb0c9f1e2e92e87d00d300
40CompilerErrors <danvgdesign@gmail.com>
2019-04-23 13:15:32 +0200

    New games

--- Golden-Experience-Gaming/database/seeds/GamesTableSeeder.php
+++ Golden-Experience-Gaming/database/seeds/GamesTableSeeder.php
@@ -26,13 +26,14 @@ class GamesTableSeeder extends Seeder
                           retro narwhal meh wolf everyday carry.',
         'publisher_id' => 4,
         'icon_url' => 'default_icon.png',
-        'image_url' => 'default_image.png'
+        'image_url' => 'game_1_image.png'
       ]);
-      $game->users()->sync(array( 
+      $game->users()->sync(array(
           2 => array('owned' => 1, 'on_cart' => 0),
           3 => array('owned' => 1, 'on_cart' => 0)
         ));
       $game->genres()->sync([13,6,5]);
+
       $game = Game::create([
         'id' => 2,
         'name' => 'Fallout 46',
@@ -52,10 +53,86 @@ class GamesTableSeeder extends Seeder
         'icon_url' => 'default_icon.png',
         'image_url' => 'default_image.png'
       ]);
-      $game->users()->sync(array( 
+      $game->users()->sync(array(
           2 => array('owned' => 2, 'on_cart' => 1),
           3 => array('owned' => 1, 'on_cart' => 0)
         ));
       $game->genres()->sync([13,6,5]);
+
+      $game = Game::create([
+        'id' => 3,
+        'name' => "JoJo's Bizarre Adventure: Last Survivor",
+        'price' => 59.99,
+        'synopsis' => "Release the power of your STAND in an epic battle
+                       against questionably dressed opponents and aztec gods of
+                       fitness",
+        'description' => 'Farm-to-table poke aesthetic, celiac humblebrag
+                          lo-fi subway tile try-hard put a bird on it waistcoat
+                          banjo coloring book mixtape bicycle rights pok pok.
+                          Wayfarers jianbing live-edge bushwick.
+                          Farm-to-table before they sold out offal,
+                          franzen mumblecore man bun lumbersexual fam.
+                          Trust fund cray kinfolk intelligentsia lumbersexual
+                          yr salvia mustache vape hashtag man bun street art
+                          whatever listicle lyft.',
+        'publisher_id' => 4,
+        'icon_url' => 'default_icon.png',
+        'image_url' => 'game_3_image.png'
+      ]);
+      $game->users()->sync(array(
+          2 => array('owned' => 1, 'on_cart' => 0),
+          3 => array('owned' => 2, 'on_cart' => 0)
+        ));
+      $game->genres()->sync([8,3]);
+
+      $game = Game::create([
+        'id' => 4,
+        'name' => "NapoopaN TotauatoT",
+        'price' => 59.95,
+        'synopsis' => "CompmoC yooy TotauatoT collecelloc wiiw thiht
+                       DefinifeD EditidE oo TotauatoT: NapoopaN, whihw inclulcni
+                       alla DUD anna feaUaef updaadpu sinnis thht gaag releeler.",
+        'description' => 'Farm-to-table poke aesthetic, celiac humblebrag
+                          lo-fi subway tile try-hard put a bird on it waistcoat
+                          banjo coloring book mixtape bicycle rights pok pok.
+                          Wayfarers jianbing live-edge bushwick.
+                          Farm-to-table before they sold out offal,
+                          franzen mumblecore man bun lumbersexual fam.
+                          Trust fund cray kinfolk intelligentsia lumbersexual
+                          yr salvia mustache vape hashtag man bun street art
+                          whatever listicle lyft.',
+        'publisher_id' => 4,
+        'icon_url' => 'default_icon.png',
+        'image_url' => 'game_4_image.png'
+      ]);
+      $game->users()->sync(array(
+          2 => array('owned' => 1, 'on_cart' => 0),
+          3 => array('owned' => 0, 'on_cart' => 1)
+        ));
+      $game->genres()->sync([11,12]);
+
+      $game = Game::create([
+        'id' => 5,
+        'name' => "GTA San Andreas: Remastered",
+        'price' => 59.95,
+        'synopsis' => "Ah shit, here we go again.",
+        'description' => 'Farm-to-table poke aesthetic, celiac humblebrag
+                          lo-fi subway tile try-hard put a bird on it waistcoat
+                          banjo coloring book mixtape bicycle rights pok pok.
+                          Wayfarers jianbing live-edge bushwick.
+                          Farm-to-table before they sold out offal,
+                          franzen mumblecore man bun lumbersexual fam.
+                          Trust fund cray kinfolk intelligentsia lumbersexual
+                          yr salvia mustache vape hashtag man bun street art
+                          whatever listicle lyft.',
+        'publisher_id' => 5,
+        'icon_url' => 'default_icon.png',
+        'image_url' => 'game_5_image.png'
+      ]);
+      $game->users()->sync(array(
+          2 => array('owned' => 2, 'on_cart' => 0),
+          3 => array('owned' => 0, 'on_cart' => 0)
+        ));
+      $game->genres()->sync([2,4,8]);
     }
 }
