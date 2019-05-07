6f5f5247069350a433976777f2d0d49490e2a2af
PabloFerrer <pablo@certrans.es>
2019-05-07 14:12:57 +0200

    Ya editamos juegos pero sin cambiar su genero

--- Golden-Experience-Gaming/resources/views/editgametext.blade.php
+++ Golden-Experience-Gaming/resources/views/editgametext.blade.php
@@ -19,13 +19,28 @@
     </div>
     @endif
 
-    <div>Elija el juego que desea editar</div>
-    <select name="games" id="games">
-        @foreach($games as $game)
-            <option value="{{ $game->id }}">{{ $game->name }}</option>
-        @endforeach
-    </select>
-    <br>
-    <button class="btn btn-default">Editar</button>
+    <form action="/editgametext/finished" method="POST">
+        {{ csrf_field() }}
+    <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" id="name" value="{{ $game->name }}" name="name">
+    </div>
+    <div class="form-group">
+        <label for="price">Price:</label>
+        <input type="int" class="form-control" id="price" value="{{ $game->price }}" name="price">
+    </div>
+    <div class="form-group">
+        <label for="description">Description:</label>
+        <input type="text" class="form-control" name="description" id="description" value="{{ $game->description }}">
+    </div>
+    <div class="form-group">
+        <label for="synopsis">Synopsis:</label>
+        <input type="text" class="form-control" id="synopsis" value="{{ $game->synopsis }}" name="synopsis">
+    </div>
+        <input type="hidden" value="{{$game->id}}" id="id" name="id">
+    <button type="submit" class="btn btn-default">Edit</button>
+    </form>
+
+
     
 @endsection
